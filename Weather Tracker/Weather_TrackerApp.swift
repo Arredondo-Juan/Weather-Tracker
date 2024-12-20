cf152be0a9a6d034f6c386bd7b0b6a41cb29ed00
Arredondo-Juan <juan_arredondo@icloud.com>
2024-12-19 22:30:55 -0600

    Initial Commit

---
+++ Weather Tracker/Weather_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TrackerApp.swift
+//  Weather Tracker
+//
+//  Created by Juan Arredondo on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
